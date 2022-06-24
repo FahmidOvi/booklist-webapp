6da1bb1dba716f2cf13196f17fab227d9fe79acf
Fahmid Ovi <fahmidovi9@gmail.com>
2022-06-24 18:50:19 -0400

    Added remote URI for MongoDB Atlas

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://127.0.0.1/books229";
-exports.RemoteURI = "your remote host information goes here";
+exports.RemoteURI = "mongodb+srv://fahmidovi:q4mzHApmfhlIGdcY@cluster0.3inb0x2.mongodb.net/media?retryWrites=true&w=majority";
 exports.HostName = "LocalHost";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
